0ad1fe78e8a1cb1ffd6e19237cd4a67ed66aaa1e
Hazel0928 <Hazel0928@users.noreply.github.com>
2024-04-01 15:23:15 +0000

    Automated deployment: Mon Apr  1 15:23:15 UTC 2024 d6851b3bfbea2d091001d37a1d0b5d2b9976f46e

---
+++ _astro/hoisted.DWKec-74.js
@@ -0,0 +1,1 @@
+import"./markdown-toc.CVbZTTvH.js";import"./hoisted.DmIRBjKd.js";import"./hoisted.DGO6Pl-U.js";import"./Tabs.astro_astro_type_script_index_0_lang.BqK4QFew.js";import"./index.DImtQWSj.js";const o=new URL(window.location.href),r=o.searchParams.get("source"),t=document.querySelector("#back-btn a");t.href=`/${r}/`;
